dd4adb692dd9b210bf9af1bb78b581d390eb465a
Khalil <khalilfoulks@gmail.com>
2020-10-13 17:35:02 -0400

    finished step 7

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -18,18 +18,17 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.pc = 0 # program counter, pointer to the currently executing instruction
-        self.ram = [0] * 256 # memory
+        self.ram = [0] * 256 # memory; 256 bits/slots 
         self.reg = [0] * 8 # register
     
-    def ram_read(self, mar):
-        value_in_mem = self.ram[mar]
+    def ram_read(self, address):
+        value_in_mem = self.ram[address]
 
         return value_in_mem
         
-    # MAR contains the address that is being read or written to; MDR contains the data that was read or the data to write
-    def ram_write(self, mar, mdr):
-        self.ram[mar] = mdr
-        return self.ram[mar]
+    def ram_write(self, address, value):
+        self.ram[address] = value
+        return self.ram[address]
 
 
     def load(self):
@@ -39,19 +38,49 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000, # operand
-            0b00001000, # operand
-            0b01000111, # PRN R0
-            0b00000000, # operand
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000, # operand
+        #     0b00001000, # operand
+        #     0b01000111, # PRN R0
+        #     0b00000000, # operand
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+        #----------------------------------------------
+        if len(sys.argv) != 2:
+            print("usage: comp.py progname")
+            sys.exit(1)
+
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    line = line.strip()
+                    # print(line)
+                    # skip blank lines or comments
+                    if line == '' or line[0] == "#":
+                        continue
+
+                    try:
+                    	str_value = line.split("#")[0]
+                        # converts binary string from base 2 to int
+                    	value = int(str_value, 2)
+
+                    except ValueError:
+                    	print(f"Invalid number: {str_value}")
+                    	sys.exit(1)
+
+                    self.ram[address] = value
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"File not found: {sys.argv[1]}")
+            sys.exit(2)
+
 
 
     def alu(self, op, reg_a, reg_b):
@@ -91,6 +120,8 @@ class CPU:
             ir = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
+            
+            # self.trace()
 
             if ir == HLT:
                 running = False
@@ -101,7 +132,7 @@ class CPU:
                 self.pc += 2
 
             elif ir == LDI:
-                self.ram[operand_a] = operand_b
+                self.ram_write(operand_a, operand_b)
                 self.pc += 3
 
             else:
